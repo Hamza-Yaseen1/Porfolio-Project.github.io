5d124e1d7eee0d9c614a7f87381073154db694c0
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2024-07-24 13:18:44 +0500

    client counter added

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ const toggleNav = () => {
   mobileNav.classList.toggle("hamburger-active");
 };
 mobileNav.addEventListener("click", () => toggleNav());
+
+
+// counter js start here
+
+        document.addEventListener('DOMContentLoaded', () => {
+            const counters = document.querySelectorAll('.counter-number');
+            counters.forEach(counter => {
+                const duration = parseInt(counter.getAttribute('data-duration'));
+                const toValue = parseInt(counter.getAttribute('data-to-value'));
+                const fromValue = parseInt(counter.getAttribute('data-from-value'));
+                const delimiter = counter.getAttribute('data-delimiter');
+                
+                let currentNumber = fromValue;
+                const increment = (toValue - fromValue) / (duration / 10);
+                
+                const updateCounter = () => {
+                    currentNumber += increment;
+                    if (currentNumber >= toValue) {
+                        currentNumber = toValue;
+                        clearInterval(interval);
+                    }
+                    counter.textContent = Math.floor(currentNumber).toLocaleString();
+                };
+
+                const interval = setInterval(updateCounter, 10);
+            });
+        });
